12176d81a641af77ef3d25bcb7d0a420015edd59
Radek Krahl <radek@krahl.pl>
2020-04-09 13:49:31 +0200

    Init.

---
+++ ClassLibrary1/MathParser.fs
@@ -0,0 +1,33 @@
+﻿namespace ClassLibrary1
+
+module MathParser =
+    open FParsec
+
+    type public Expr =
+        | Add of Expr*Expr
+        | Sub of Expr*Expr
+        | Val of float
+
+    let private ws = spaces
+
+    let private ch c = skipChar c >>. ws
+
+    let private number: Parser<Expr, unit> = pfloat .>> ws |>> (fun x -> Val(x))
+
+    let private opp = new OperatorPrecedenceParser<_,_,_>()
+
+    let private expr = opp.ExpressionParser
+
+    opp.TermParser <- choice [number]
+   
+    opp.AddOperator(InfixOperator("+", ws, 1, Associativity.Left, fun x y -> Add(x,y)))
+    opp.AddOperator(InfixOperator("-", ws, 1, Associativity.Left, fun x y -> Sub(x,y)))
+
+    let private completeExpression = ws >>. expr .>> eof
+
+    let public parse s = 
+        let result = run completeExpression s
+        match result with
+            | Success(r,_,_) -> r
+            | Failure(f,_,_) -> failwith (sprintf "%A" f)
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var formula = "2 + 1 - 4";
+            var expr = ClassLibrary1.MathParser.parse(formula);
+        }
+    }
+}
